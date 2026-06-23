69aa52e2a9ba89c805541860cbe31b7c2cfcf854
leonayzacayo <leonayzacayo@email.com>
2026-06-23 11:39:33 -0400

    fix: switch staticfiles storage to CompressedStaticFilesStorage to prevent MissingFileError in Whitenoise

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -192,8 +192,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [FRONTEND_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-WHITENOISE_MANIFEST_STRICT = False
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
